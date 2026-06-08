a15aa52ded9cee0d6751eb0f15c9292697f85baa
Sebastian Conn <sebastian.conn@rmit.edu.au>
2026-06-09 09:38:35 +1000

    Fix editor: use imperative property binding for ha-form

--- src/editor.ts
+++ src/editor.ts
@@ -9,57 +9,47 @@ const SCHEMA = [
   { name: 'dryer_door_entity', selector: { entity: { domain: 'binary_sensor' } } },
   { name: 'washer_label', selector: { text: {} } },
   { name: 'dryer_label', selector: { text: {} } },
-  { name: 'color_running', selector: { color_rgb: {} } },
-  { name: 'color_finished', selector: { color_rgb: {} } },
-  { name: 'color_idle', selector: { color_rgb: {} } },
+  { name: 'color_running', selector: { text: {} } },
+  { name: 'color_finished', selector: { text: {} } },
+  { name: 'color_idle', selector: { text: {} } },
   { name: 'show_power', selector: { boolean: {} } },
   { name: 'show_time', selector: { boolean: {} } },
   { name: 'show_labels', selector: { boolean: {} } },
-  { name: 'svg_width', selector: { number: { min: 80, max: 200, step: 10 } } },
-  { name: 'svg_height', selector: { number: { min: 120, max: 250, step: 10 } } },
-  { name: 'fill_opacity', selector: { number: { min: 0, max: 1, step: 0.1 } } },
+  { name: 'svg_width', selector: { number: { min: 80, max: 200, step: 10, mode: 'slider' } } },
+  { name: 'svg_height', selector: { number: { min: 120, max: 250, step: 10, mode: 'slider' } } },
+  { name: 'fill_opacity', selector: { number: { min: 0, max: 1, step: 0.1, mode: 'slider' } } },
 ];
 
 class LaundryStatusCardEditor extends HTMLElement {
   private _config!: LaundryCardConfig;
   private _hass: any;
+  private _form: any;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this._form = document.createElement('ha-form');
+    this._form.computeLabel = (s: any) => s.name.replace(/_/g, ' ').replace(/\b\w/g, (c: string) => c.toUpperCase());
+    this._form.addEventListener('value-changed', (ev: any) => {
+      const event = new CustomEvent('config-changed', {
+        detail: { config: ev.detail.value },
+        bubbles: true,
+        composed: true,
+      });
+      this.dispatchEvent(event);
+    });
+    this.shadowRoot!.appendChild(this._form);
+  }
 
   setConfig(config: LaundryCardConfig) {
     this._config = config;
-    this.render();
+    this._form.data = config;
+    this._form.schema = SCHEMA;
   }
 
   set hass(hass: any) {
     this._hass = hass;
-    this.render();
-  }
-
-  private render() {
-    if (!this._config || !this._hass) return;
-    if (!this.shadowRoot) this.attachShadow({ mode: 'open' });
-
-    this.shadowRoot!.innerHTML = `<ha-form
-      .hass=${this._hass}
-      .data=${this._config}
-      .schema=${SCHEMA}
-      .computeLabel=${(s: any) => s.name.replace(/_/g, ' ').replace(/\b\w/g, (c: string) => c.toUpperCase())}
-      @value-changed=${this._valueChanged}
-    ></ha-form>`;
-
-    const form = this.shadowRoot!.querySelector('ha-form') as any;
-    if (form) {
-      form.hass = this._hass;
-      form.data = this._config;
-      form.schema = SCHEMA;
-      form.computeLabel = (s: any) => s.name.replace(/_/g, ' ').replace(/\b\w/g, (c: string) => c.toUpperCase());
-      form.addEventListener('value-changed', this._valueChanged.bind(this));
-    }
-  }
-
-  private _valueChanged(ev: CustomEvent) {
-    const config = ev.detail.value;
-    const event = new CustomEvent('config-changed', { detail: { config }, bubbles: true, composed: true });
-    this.dispatchEvent(event);
+    this._form.hass = hass;
   }
 }
 
